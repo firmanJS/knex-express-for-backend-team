ff9798bd5f79b1c7cade102c9a36eed737e177eb
root <fimzhakim@gmail.com>
2022-11-28 08:54:00 +0700

    add new coverage

--- __test__/unit/constant.test.js
+++ __test__/unit/constant.test.js
@@ -1,26 +1,33 @@
+/* eslint-disable no-undef */
 const { HTTP } = require('../../src/utils/constant')
 
 describe('all constant variable testing', () => {
   // test for HTTP
-  it('BAD REQUEST VALUE ', () => {
-    expect(HTTP.BAD_REQUEST).toBe(400)
-  })
-  it('HTTP CREATED', () => {
+  it('CREATED', () => {
     expect(HTTP.CREATED).toBe(201)
   })
-  it('FORBIDDEN PAGE', () => {
-    expect(HTTP.FORBIDDEN).toBe(403)
-  })
-  it('NOTFOUND CODE', () => {
-    expect(HTTP.NOT_FOUND).toBe(404)
-  })
-  it('HTTP OK', () => {
+  it('SUCCESS', () => {
     expect(HTTP.OK).toBe(200)
   })
-  it('INTERNAL SERVER ERRROR', () => {
-    expect(HTTP.INTERNAL_SERVER_ERROR).toBe(500)
+  it('ACCEPTED', () => {
+    expect(HTTP.ACCEPTED).toBe(202)
+  })
+  it('BAD_REQUEST', () => {
+    expect(HTTP.BAD_REQUEST).toBe(400)
   })
   it('UNAUTHORIZED', () => {
     expect(HTTP.UNAUTHORIZED).toBe(401)
+  })
+  it('FORBIDDEN', () => {
+    expect(HTTP.FORBIDDEN).toBe(403)
+  })
+  it('NOT_FOUND', () => {
+    expect(HTTP.NOT_FOUND).toBe(404)
+  })
+  it('UNPROCESSABLE_ENTITY', () => {
+    expect(HTTP.UNPROCESSABLE_ENTITY).toBe(422)
+  })
+  it('INTERNAL_SERVER_ERROR', () => {
+    expect(HTTP.INTERNAL_SERVER_ERROR).toBe(500)
   })
 })
---
+++ __test__/unit/custome.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+const custom = require('../../src/utils/custom')
+
+describe('all custom function testing', () => {
+  it('is numeric function', () => {
+    expect(custom.isNumeric('2')).toBe(true)
+  })
+  it('is numeric function', () => {
+    expect(custom.isNumeric(2)).toBe(false)
+  })
+  it('is slug function', () => {
+    expect(custom.convertToSlug('testing data')).toBe('testing-data')
+  })
+  it('is first upper string function', () => {
+    expect(custom.ucword('testing')).toBe('Testing')
+  })
+  it('is currency function', () => {
+    expect(custom.formatCurrency(2000)).toBe(custom.formatCurrency(2000))
+  })
+})
