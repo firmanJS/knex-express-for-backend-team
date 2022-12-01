168e45e3310f3adf8e26215fe14206c9713f879e
root <fimzhakim@gmail.com>
2022-12-01 08:23:48 +0700

    raise up coverage

--- __test__/unit/custom.test.js
+++ __test__/unit/custom.test.js
@@ -1,9 +1,32 @@
 /* eslint-disable no-undef */
+const httpMock = require('node-mocks-http')
 const custom = require('../../src/utils/custom')
 
+let req
+// let res
+// let next
+
 describe('all custom function testing', () => {
+  beforeEach(() => {
+    // create express request and response mock
+    req = httpMock.createRequest()
+    // res = httpMock.createResponse()
+    // next = jest.fn()
+  })
+  it('is paramsHttp', () => {
+    expect(custom.paramsHttp(req)).toStrictEqual({})
+  })
+  it('is queryHttp', () => {
+    expect(custom.queryHttp(req)).toStrictEqual({})
+  })
+  it('is bodyHttp', () => {
+    expect(custom.bodyHttp(req)).toStrictEqual({})
+  })
   it('is numeric function', () => {
     expect(custom.isNumeric('2')).toBeTruthy()
+  })
+  it('is numeric function', () => {
+    expect(custom.isNumeric(2)).toBe(false)
   })
   it('is slug function', () => {
     expect(custom.convertToSlug('testing data')).toBe('testing-data')
