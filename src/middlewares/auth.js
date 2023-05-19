6f89758f98400c19ea611c56daa777f463170f29
firmanjs <fimzhakim@gmail.com>
2023-05-19 11:06:06 +0700

    fixing middleware token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,38 +3,25 @@ const { JWT_ALGORITHM, JWT_SECRET_KEY } = require('../config');
 const { HTTP, baseResponse, mappingError } = require('../utils');
 const { lang } = require('../lang');
 
-const validate = (req, res, next) => {
-  const token = req?.headers?.authorization.split(' ')[1];
-  const credential = jwt.verify(token, JWT_SECRET_KEY, { algorithms: JWT_ALGORITHM });
-  if (credential) {
-    req.users_info = credential;
-    return next();
+const result = {
+  code: HTTP.UNAUTHORIZED,
+  data: {
+    status: false,
+    message: lang.__('validator.required', { field: 'token' }),
+    data: [],
   }
-  const result = {
-    code: HTTP.UNAUTHORIZED,
-    data: {
-      status: false,
-      message: lang.__('validator.required', { field: 'token' }),
-      data: [],
-    }
-  };
-  return baseResponse(res, result);
 };
 
 exports.verifyToken = (req, res, next) => {
   try {
-    if (!req.headers.authorization) {
-      const result = {
-        code: HTTP.UNAUTHORIZED,
-        data: {
-          status: false,
-          message: lang.__('validator.required', { field: 'token' }),
-          data: [],
-        }
-      };
-      return baseResponse(res, result);
+    if (!req.headers.authorization) return baseResponse(res, result);
+    const token = req?.headers?.authorization.split(' ')[1];
+    const credential = jwt.verify(token, JWT_SECRET_KEY, { algorithms: JWT_ALGORITHM });
+    if (credential) {
+      req.users_info = credential;
+      return next();
     }
-    return validate(req, res, next);
+    return baseResponse(res, result);
   } catch (error) {
     const err = mappingError(req, error, HTTP.UNAUTHORIZED);
     return baseResponse(res, err);
