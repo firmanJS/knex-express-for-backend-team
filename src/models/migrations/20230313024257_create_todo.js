1ed5fc9b138183253d0fd02d7c17330d9d2c1e8d
firmanjs <fimzhakim@gmail.com>
2023-12-12 08:33:30 +0700

    fix: data sample

--- src/models/migrations/20230313024257_create_todo.js
+++ src/models/migrations/20230313024257_create_todo.js
@@ -1,16 +1,17 @@
 const { TABLES } = require('../schema');
 
-exports.up = async (knex) => knex.schema.createTable(TABLES.TODO, (table) => {
-  table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
-  table.string('name', 150).notNullable().unique();
-  table.text('description');
-  table.timestamp('created_at').defaultTo(knex.fn.now());
-  table.uuid('created_by');
-  table.timestamp('updated_at').defaultTo(knex.fn.now());
-  table.uuid('updated_by');
-  table.timestamp('deleted_at');
-  table.uuid('deleted_by');
-});
+exports.up = async (knex) =>
+  knex.schema.createTable(TABLES.TODO, (table) => {
+    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
+    table.string('name', 150).notNullable().unique();
+    table.text('description');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.uuid('created_by');
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.uuid('updated_by');
+    table.timestamp('deleted_at');
+    table.uuid('deleted_by');
+  });
 
 /**
  * @param { import("knex").Knex } knex
--- src/models/migrations/20231211021353_create_trigger_update_todo.js
+++ src/models/migrations/20231211021353_create_trigger_update_todo.js
@@ -1,7 +1,7 @@
 const { TRIGGER, FUNC, TABLES } = require('../schema');
 
-exports.up = async (knex) => knex.schema
-  .raw(`create trigger ${TRIGGER.UPDATE_TODO} after update on
+exports.up = async (knex) =>
+  knex.schema.raw(`create trigger ${TRIGGER.UPDATE_TODO} after update on
       ${TABLES.TODO} for each row execute function ${FUNC.UPDATE_TODO};
   `);
 
@@ -9,6 +9,5 @@ exports.up = async (knex) => knex.schema
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = (knex) => knex.schema.raw(
-  `DROP TRIGGER ${TRIGGER.UPDATE_TODO} ON ${TABLES.TODO}`
-);
+exports.down = (knex) =>
+  knex.schema.raw(`DROP TRIGGER ${TRIGGER.UPDATE_TODO} ON ${TABLES.TODO}`);
